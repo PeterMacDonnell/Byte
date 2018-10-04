fd3e931df2f9402a05e9e08d0c7728ffdd27a094
adorkwithaspork <gracehoughton@gmail.com>
2018-10-04 15:04:55 -0700

    push

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const routes = require("./routes");
 
-
+var app = express()
+var PORT = 3001;
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Add routes, both API and view
-app.use(routes);
+// app.use(routes);
 
 // Start the API server
 app.listen(PORT, function() {
