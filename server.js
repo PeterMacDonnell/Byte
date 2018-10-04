3b99edaa9dc6fba3a67f53916234401b52a60350
PeterMacDonnell <petermacdonnell@berkeley.edu>
2018-10-04 12:50:47 -0700

    changed locations

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const routes = require("../../../routes");
+const routes = require("./routes");
 
 
 // Define middleware here
