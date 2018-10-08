f81cdca1c56caa583da8bf466b68f57e7e3c0182
PeterMacDonnell <petermacdonnell@berkeley.edu>
2018-10-08 12:07:33 -0700

    Some schemas added, controller started

---
+++ models/users.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const users = new Schema({
+    //not sure what to fill this in with considering google oauth
+  restName: { type: String, required: true },
+  isChosen: { type: Boolean, required: true }
+
+});
+
+const User = mongoose.model("Choice", users);
+
+module.exports = User;
