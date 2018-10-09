1c33c0deecbe2a4d8fa3c85a776678b666183471
adorkwithaspork <gracehoughton@gmail.com>
2018-10-09 11:27:37 -0700

    adding jsx

--- client/src/Components/Card/Card.js
+++ client/src/Components/Card/Card.js
@@ -7,7 +7,7 @@ const Card = props =>
         <div className="card-body">
           <h5 className="card-title text-center">Whoa. You got a byte!</h5>
           <h4 className="card-title text-center">{props.restaurant.name}</h4>
-          <h4 className="card-title text-center">{props.genere.name}</h4>
+          <h4 className="card-title text-center">{props.genre.name}</h4>
           <h4 className="card-title text-center">{props.price.name}</h4>
           <h4 className="card-title text-center">{props.distance.name}</h4>
           <p className="card-text">
--- client/src/Components/Match/Match.js
+++ client/src/Components/Match/Match.js
@@ -1,5 +1,26 @@
 import React from "react";
+var NewComponent = React.createClass({
+      render: function() {
+        return (
+    
+          <div className="card" style={{width: '18rem'}}>
+            <img className="card-img-top" src="http://sffood.net/wp-content/uploads/top-dog-hot-dogs-27-700x452.jpg" alt="Card image cap" />
+            <div className="card-body">
+              <h5 className="card-title">Top Dog</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" className="btn btn-success">Yes</a>
+              <a href="#" className="btn btn-danger">No</a>
+           {/* If can animate, put this on the back
+               <a href="topdoghotdogs.com" class="btn btn-primary">Website</a>
+          <a href="http://topdoghotdogs.com/themenu.html" class="btn btn-primary">Menu</a>
+          <a href="#" class="btn btn-primary">Directions</a>  */}
+            </div>
+          </div>
+        );
+      }
+    });
 
+    
 
       
       
