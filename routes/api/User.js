c7745db3d6971c32391879150735745ac80f5dd2
PeterMacDonnell <petermacdonnell@berkeley.edu>
2018-10-09 14:20:22 -0700

    routes added for each controller, incomplete. Controllers added for each schema.

---
+++ routes/api/User.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+const firstController = require("../../controllers/UserController")
+// Matches with "/api/items"
+router.route("/home")
+  .get(UserController.findById)
+  .post(UserController.create);
+
+// Matches with "/api/items/:id"
+router.route("/:id")
+  .get(UserController.findById)
+  .put(UserController.update)
+  .delete(UserController.remove);
+
+module.exports = router;
