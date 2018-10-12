0681434cd93f5e2f143eac58464fad4dd4dd554d
adorkwithaspork <gracehoughton@gmail.com>
2018-10-12 10:48:32 -0700

    davids fixes and tweaks

--- client/src/Components/Choice/Choiceone.js
+++ client/src/Components/Choice/Choiceone.js
@@ -5,20 +5,16 @@ import{ firebase } from "../../firebase/index";
 import './Choice.css';
 import {Col, Container, Row} from '../Grid/index';
 // import {Card} from '../Choice/Card';
-
-
 class Choiceone extends React.Component {
- constructor(props){
-   super(props)
- 
-   this.state = {
+
+   state = {
       array_of_places: [],
       place_id: [],
-      detailed_array: [],
+      detailedArray: [],
       places_review: []
       // eslint-disable-next-line
     }
-  }
+  
   
   
   api_call_function () {
@@ -29,7 +25,7 @@ class Choiceone extends React.Component {
     .then(res => {
       const array_of_places = res.data.results;
       const places_id = array_of_places.map(place => place.place_id);
-      this.setState({array_of_places});
+      this.setState({array_of_places: array_of_places});
       this.api_places_details(places_id);
     });
   }
@@ -43,46 +39,56 @@ class Choiceone extends React.Component {
       .then(res => {
         console.log(res)
         place_holder.push(res.data.result)
-      
-        // const text_review = res.data.result.reviews;
-        // text_review.forEach(element=>{
-        //   const places_review = element.text;
-        //   this.setState({places_review:element.text});
-        //   // console.log(places_review);
-
-        // })
-        
+        this.setState({detailedArray: place_holder});
         })
-
-        // console.log(res.data.result.reviews);
-        // put a loopydo in
-  
       })
-      this.setState({detailed_array: place_holder});
+      
     };
-
-  
 
   componentDidMount() {
     this.api_call_function();
   }
 
-  // getData(){
-  //   const detailed_array = this.state.detailed_array;
-  //   console.log(detailed_array);
-  // }
-
   render() {
-   
+    const api_key = "AIzaSyA7KHhrTUzj_S8Vo1hiPjVMsZKdXKfzpv4";
     return (
       <div>
-      <ul>
-      
-      {this.state.detailed_array.map(data => {
-        return <li>{data.name}</li>
-        }
-      )}
-      </ul>
+      <h1> Hello People Of The Internet!</h1>
+      {this.state.detailedArray.length ? (
+            <div>
+                {this.state.detailedArray.map(chicken => (
+                  <Container> 
+      <Row >
+          {this.state.detailedArray.map(place=>(
+      <Col size="3">
+           <div className="card wholecard" style={{width: '18rem', height: '30rem'}}>
+            <img className="card-img-top" style={{width: '18rem', height: '30rem'}} src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${place.photos[0].photo_reference}&key=${api_key}`} alt="Restaurant" />
+            <div className="card-body">
+              <h5 className="card-title">{place.name}</h5>
+              <p className="card-text text-center">Rating: {place.rating}</p>
+              <p className="card-text text-center">price: {place.price_level}</p>
+              <p className="card-text text-center">address: {place.formatted_address}</p>
+              <p className="card-text text-center">Phone: {place.formatted_phone_number}</p>
+              <p className="card-text text-center">reviews: {place.reviews[0].text}</p>
+              <p className="card-text text-center">Price Level: {place.price_level}</p>
+              <a href="should be a 1" className="btn btn-success">Yes</a>
+              <a href="should be a 0" className="btn btn-danger">No</a>
+            </div>
+          </div>
+        </Col>
+      ))}
+       </Row>
+        </Container>
+                    
+                    
+                    
+                  
+                ))}
+            </div>
+              
+            ) : (
+              <h3>No Results to Display</h3>
+            )}
      </div>
     )     
     };
