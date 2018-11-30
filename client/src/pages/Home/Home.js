3ef8f5852c57d0350278236b56df3295ca9dd6a8
adorkwithaspork <gracehoughton@gmail.com>
2018-11-30 15:37:20 -0800

    took out loop

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -15,15 +15,15 @@ class Home extends Component {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700|Raleway" rel="stylesheet" />
-        <link rel="stylesheet" href="css/bootstrap.min.css" />
-        <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css" />
-        <link rel="stylesheet" href="css/animate.css" />
-        <link rel="stylesheet" href="css/owl.carousel.min.css" />
-        <link rel="stylesheet" href="css/owl.theme.default.min.css" />
-        <link rel="stylesheet" href="css/magnific-popup.css" />
-        <link rel="stylesheet" href="css/bootstrap-datepicker.css" />
-        <link rel="stylesheet" href="css/jquery.timepicker.css" />
-        <link rel="stylesheet" href="css/icomoon.css" />
+          <link rel="stylesheet" href="css/bootstrap.min.css" />
+          <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css" />
+          <link rel="stylesheet" href="css/animate.css" />
+          <link rel="stylesheet" href="css/owl.carousel.min.css" />
+          <link rel="stylesheet" href="css/owl.theme.default.min.css" />
+          <link rel="stylesheet" href="css/magnific-popup.css" />
+          <link rel="stylesheet" href="css/bootstrap-datepicker.css" />
+          <link rel="stylesheet" href="css/jquery.timepicker.css" />
+          <link rel="stylesheet" href="css/icomoon.css" />
         <link rel="stylesheet" href="css/style.css" />
         <nav className="navbar navbar-expand-lg navbar-dark site_navbar bg-dark site-navbar-light" id="site-navbar">
           <div className="container">
--- client/src/pages/RoomPage/RoomPage.js
+++ client/src/pages/RoomPage/RoomPage.js
@@ -58,9 +58,9 @@ class RoomPage extends React.Component {
     return (
       <div>
       <h1> Welcome to The Byte App!</h1>
-      {this.state.detailedArray.length ? (
+      {/* {this.state.detailedArray.length ? ( */}
             <div>
-                {this.state.detailedArray.map(chicken => (
+                {/* {this.state.detailedArray.map(chicken => ( */}
                   <Container> 
       <Row >
           {this.state.detailedArray.map(place=>(
@@ -83,75 +83,18 @@ class RoomPage extends React.Component {
        </Row>
         </Container>
                     
-                    
-                    
+            </div>  
+            </div>      
                   
-                ))}
-            </div>
-              
-            ) : (
-              <h3>No Results to Display</h3>
-            )}
-     </div>
-    )     
-    };
-}
+    )        
+  }
+};
 
 export default RoomPage;
 
-// import React from 'react';
-// import axios from 'axios';
-// import Choiceone from '../../Components/Choice/Choiceone';
-// import Navbar from '../../Components/Navbar/Navbar';
-// import  firebase  from "../../firebase/index";
-
-// export default class roomPage extends React.Component {
-//   state = {
-//     array_of_places: [],
-//     place_id: []
-//   }
-  
-
-//   api_call_function () {
-//     const food_input = "restaurants"
-//     const location_input = "berkeley"
-//     const api_key = "AIzaSyA7KHhrTUzj_S8Vo1hiPjVMsZKdXKfzpv4"
-//     axios.get("https://cors-anywhere.herokuapp.com/" + "https://maps.googleapis.com/maps/api/place/textsearch/json?query=+"+food_input+"+in+"+location_input+"+&key="+api_key)
-//     .then(res => {
-//       const array_of_places = res.data.results;
-//       const places_id = array_of_places.map(place => place.place_id);
-//       this.setState({array_of_places})
-//       this.api_places_details(places_id);
-//     })
-//   }
-//   api_places_details(places_id){
-//     const api_key = "AIzaSyA7KHhrTUzj_S8Vo1hiPjVMsZKdXKfzpv4";
-//     // console.log(places_id);
-//     places_id.forEach(element => {
-//       axios.get("https://cors-anywhere.herokuapp.com/"+"https://maps.googleapis.com/maps/api/place/details/json?placeid="+element+"&fields=photos,name,rating,formatted_phone_number,review&key="+api_key)
-//       .then(res => {
-//         const text_review = res.data.result.reviews;
-//         text_review.forEach(element=>{
-//           const places_review = element.text;
-//           // console.log(places_review)
-//         })
-
-//         // console.log(res.data.result.reviews);
-//         // put a loopydo in
-  
-//       })
-//     });
-
-//   }
-
-//   componentDidMount() {
-//     this.api_call_function()
-//   }
-//   render() {
-//     return (
-//       <div>
-//         <Choiceone />   
-//         </div>
-//     );
-//   }
-// };
+           
+{/*               
+            ) : (
+              <h3>No Results to Display</h3>
+            )} */}
+     {/* </div> */}
