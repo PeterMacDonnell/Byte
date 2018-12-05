3525e80bf4aba2db83771ddceb52bdb0480456cf
Crystal Ngo <crystalngo88@gmail.com>
2018-12-04 23:42:20 -0800

    Carlos' code

--- client/src/pages/RoomPage/RoomPage.js
+++ client/src/pages/RoomPage/RoomPage.js
@@ -11,7 +11,8 @@ class RoomPage extends React.Component {
       array_of_places: [],
       place_id: [],
       detailedArray: [],
-      places_review: []
+      places_review: [],
+      crystalsFavoriteCoffee: 'starbucks'
     }
   api_call_function () {
     const food_input = "restaurants";
@@ -26,14 +27,14 @@ class RoomPage extends React.Component {
       this.api_places_details(places_id);
       console.log(array_of_places,"AP");
     })
-  } 
+  }
 
   api_places_details(places_id) {
     const api_key = "AIzaSyA7KHhrTUzj_S8Vo1hiPjVMsZKdXKfzpv4";
-    const place_holder = []; 
+    const place_holder = [];
     // console.log(places_id);
     places_id.forEach(element => {
-      axios.get("https://cors-anywhere.herokuapp.com/"+"https://maps.googleapis.com/maps/api/place/details/json?placeid="+element+"&fields=photo,name,rating,price_level,vicinity,id,formatted_phone_number,scope,type,opening_hours,website,review&key="+api_key)
+      axios.get("https://cors-anywhere.herokuapp.com/"+"https://maps.googleapis.com/maps/api/place/details/json?placeid="+element+"&fields=photo,name,rating,price_level,formatted_address,id,formatted_phone_number,scope,type,opening_hours,website,review&key="+api_key)
       .then(res => {
         console.log(res)
         place_holder.push(res.data.result)
@@ -43,16 +44,35 @@ class RoomPage extends React.Component {
     };
 
 
-    addVotes = element => {
-      // element.preventDefault();
-      const db = firebase.firestore();
-      db.settings({
-        timestampsInSnapshots: true
-      });
-      const elementRef = db.collection("Users").add({
-        id: this.state.place_id
-      });  
-    };
+  addVotes = (place, vote, index) => {
+    // REMOVE CARD FROM PAGE:
+
+    // traditional method
+    const oldDetailedArray = this.state.detailedArray;
+    const newDetailedArray = oldDetailedArray.slice(0, index).concat(oldDetailedArray.slice(index + 1));
+    this.setState({detailedArray: newDetailedArray});
+
+    // official advanced method
+    // this.setState((prevState) => {
+    //   const oldDetailedArray = prevState.detailedArray;
+    //   const newDetailedArray = oldDetailedArray.slice(0, index).concat(oldDetailedArray.slice(index + 1));
+    //   return {
+    //     detailedArray: newDetailedArray,
+    //   };
+    // });
+
+
+    // FIREBASE SECTION
+
+    const db = firebase.firestore();
+    db.settings({
+      timestampsInSnapshots: true
+    });
+    const elementRef = db.collection("Users").add({
+      id: place.id, // "f716a951b4294c0b03a97d4ae1414408dc254ad3"
+      vote: place.vote, // 'yes' or 'no'
+    });
+  };
 
 
   componentDidMount() {
@@ -66,33 +86,33 @@ class RoomPage extends React.Component {
     console.log('in RoomPage, props:', this.props);
 
     const api_key = "AIzaSyA7KHhrTUzj_S8Vo1hiPjVMsZKdXKfzpv4";
-
+    console.log('this.state.detailedArray:', this.state.detailedArray)
     return (
       <div>
-      <h1> Welcome to The Byte App!</h1>
-            <div>
-                  <Container> 
-      <Row>
-          {this.state.detailedArray.map(place=>(
-      <Col size="3">
-           <div className="card wholecard" style={{position: 'relative', width: '18rem', height: '30rem'}}>
-            <img className="card-img-top" style={{position: 'relative', width: '18rem', height: '10rem', borderRadius: 5}} src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${place.photos[0].photo_reference}&key=${api_key}`} alt="Restaurant" />
-            <div className="card-body" style={{lineHeight: '1'}}>
-              <h5 className="card-title">{place.name}</h5>
-              <p className="card-text text-center">Rating: {place.rating}/5</p>
-              <p className="card-text text-center">Price: {place.price_level}/4</p>
-              <p className="card-text text-center">Phone: {place.formatted_phone_number}</p>
-              <p className="card-text text-center">Address: {place.vicinity}</p>
-              <form action="" className="btn btn-default yes" method="post"> <button name="Yes" id={place.place_id} value="1" style={{fontSize: '16px', fontFamily: 'Raleway, sans serif', backgroundColor: '#D0DE4b'}} >Yes</button></form>
-              <form action="" className="btn btn-default no" method="post"> <button name="No" id={place.place_id}  value="0" style={{fontSize: '16px', fontFamily: 'Raleway, sans serif', backgroundColor: '#F6422B'}} >No</button></form>
-            </div>
-          </div>
-        </Col>
-      ))}
-       </Row>
-        </Container>
-            </div>  
-      </div>  
+        <h1> Welcome to The Byte App!</h1>
+        <div>
+          <Container>
+            <Row>
+              {this.state.detailedArray.map((place, index) => (
+                <Col size="3">
+                   <div className="card wholecard" style={{position: 'relative', width: '18rem', height: '30rem'}} onClick={() => console.log('clicked on:', place.name)}>
+                    <img className="card-img-top" style={{position: 'relative', width: '18rem', height: '10rem', borderRadius: 5}} src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${place.photos[0].photo_reference}&key=${api_key}`} alt="Restaurant" />
+                    <div className="card-body" style={{lineHeight: '1'}}>
+                      <h5 className="card-title">{place.name}</h5>
+                      <p className="card-text text-center">Rating: {place.rating}/5</p>
+                      <p className="card-text text-center">Price: {place.price_level}/4</p>
+                      <p className="card-text text-center">Phone: {place.formatted_phone_number}</p>
+                      <p className="card-text text-center">Address: {place.formatted_address}</p>
+                      <button onClick={() => this.addVotes(place, 'yes', index)} name="Yes" id={place.place_id} value="1" style={{fontSize: '16px', fontFamily: 'Raleway, sans serif', backgroundColor: '#D0DE4b'}} >Yes</button>
+                      <button onClick={() => this.addVotes(place, 'no', index)} name="No" id={place.place_id}  value="0" style={{fontSize: '16px', fontFamily: 'Raleway, sans serif', backgroundColor: '#F6422B'}} >No</button>
+                    </div>
+                  </div>
+                </Col>
+              ))}
+           </Row>
+          </Container>
+              </div>
+      </div>
     )
   }
 };
