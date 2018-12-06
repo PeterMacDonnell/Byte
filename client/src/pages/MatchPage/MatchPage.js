f5ed7a110693ae2645521213a05c37558e5821c8
Crystal Ngo <crystalngo88@gmail.com>
2018-12-06 14:08:42 -0800

    centered WaitingForAMatch card

--- client/src/pages/MatchPage/MatchPage.js
+++ client/src/pages/MatchPage/MatchPage.js
@@ -7,16 +7,11 @@ export default class roomPage extends React.Component{
     <Container>
         <Row>
             <Col size="4"/>
-            <Col size="4">
-                <div className="card wholecard" style={{width: '18rem', height: '40rem'}}>
-                    <img className="card-img-top" style={{width: '18rem', height: '30rem'}} src={"https://lh5.googleusercontent.com/p/AF1QipOrwWBES9B0t2hzBN8HOg8jITdWKVN8G8qweDpm=w203-h135-k-no"} alt="Restaurant" />
+            <Col size="4" style= {{}}>
+                <div className="card wholecard" style={{ width: '18rem', height: '25rem'}}>
+                    <img className="card-img-top" style={{width: '18rem', height: '25rem'}} src={"https://i.imgur.com/4KRisDz.png"} alt="Restaurant" />
                     <div className="card-body">
-                    <h5 className="card-title">{"Delage"}</h5>
-                    <p className="card-text text-center">Rating: {"4.5/5"}</p>
-                    <p className="card-text text-center">Price: {"3/4"}</p>
-                    <p className="card-text text-center">Phone: {"(510) 823-2050"}</p>
-                    <p className="card-text text-center">Address: {"536 9th St, Oakland, CA 94607"}</p>
-                    <p className="card-text text-center">Reviews: {"Buzzy Japanese bistro with upscale omakase-style sushi & drinks in a cozy, minimalist space."}</p>
+                    <h5 className="card-title">Thanks for using The Byte App! A match will be up shortly.</h5>
                     {/* <form action="" className="btn btn-success" method="post"> <button name="Yes" id={place.place_id} value="1">Yes</button></form>
                     <form action="" className="btn btn-success" method="post"> <button name="No" id={place.place_id}  value="0">No</button></form> */}
                     </div>
